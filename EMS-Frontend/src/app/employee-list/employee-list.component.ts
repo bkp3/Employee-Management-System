5b4743c9635ac64580d2ca6dfd33d2611afbb3dd
Bhola Kumar <bholakumar.7372@gmail.com>
2023-08-22 00:26:39 +0530

    Creating Angular Employee List Component

---
+++ EMS-Frontend/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit{
+
+  employees: Employee[] | undefined;
+  constructor(){
+
+  }
+  ngOnInit():void{
+    this.employees=[
+      {
+        id:1,
+        firstname:"Bhola",
+        lastname:"Kumar",
+        emailid:"bhola.k@samsung.com"
+      },
+      {
+        id:2,
+        firstname:"Bhola",
+        lastname:"Kumar",
+        emailid:"bhola.k@samsung.com"
+      }
+      
+    ]
+  }
+
+}
---
+++ EMS-Frontend/src/app/employee.ts
@@ -0,0 +1,7 @@
+export class Employee {
+    id: number | undefined;
+    firstname: String | undefined;
+    lastname: String | undefined;
+    emailid: String | undefined;
+
+}
