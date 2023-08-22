60a6c8b5c677b5e6034efb4133f803740411997c
Bhola Kumar <bholakumar.7372@gmail.com>
2023-08-22 18:46:16 +0530

    Routing and Navigation

--- EMS-Frontend/src/app/app-routing.module.ts
+++ EMS-Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+// Steps 4. We need to configure routes in routes array
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },
+  { path: '', redirectTo: 'employees', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+// Steps 2. We need to create AppRoutingModule
 export class AppRoutingModule { }
--- EMS-Frontend/src/app/app.module.ts
+++ EMS-Frontend/src/app/app.module.ts
@@ -4,14 +4,19 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    // Steps 3 : We need to add app-routing.modules.ts here
     AppRoutingModule,
     HttpClientModule
   ],
